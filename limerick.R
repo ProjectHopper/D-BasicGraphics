509d7bb1b54cb7a5fe96c1d526d9b07f0f2dd7b5
DragonflyStats <kobriendublin@gmail.com>
2017-08-26 12:39:03 +0100

    Update limerick.R

--- limerick.R
+++ limerick.R
@@ -1,3 +1,5 @@
 source("https://gist.githubusercontent.com/DragonflyStats/305040862a6f041812a3baade5d27f50/raw/ba4c147eb6e2ba582bbd0d88bd30e8afce24d970/Limerick.R")
 
 cappun <- read.csv("https://raw.githubusercontent.com/RWorkshop/workshopdatasets/master/cappun.csv")
+
+diet <- source("https://gist.githubusercontent.com/DragonflyStats/9a6c2f42b167580ab5fa06adc90c65b9/raw/b1537e3663e00b1269624c710b98d83b3b1e4598/createDietData.R")
