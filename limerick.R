7dfefe59860cfe317e5e73dc47c0b299b588d441
DragonflyStats <kobriendublin@gmail.com>
2017-08-26 10:30:09 +0100

    Update limerick.R

--- limerick.R
+++ limerick.R
@@ -1,2 +1,3 @@
 source("https://gist.githubusercontent.com/DragonflyStats/305040862a6f041812a3baade5d27f50/raw/ba4c147eb6e2ba582bbd0d88bd30e8afce24d970/Limerick.R")
 
+cappun <- read.csv("https://raw.githubusercontent.com/RWorkshop/workshopdatasets/master/cappun.csv")
