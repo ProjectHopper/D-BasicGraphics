414e41cf7537e4bd7eb1db709c3beb3b1758ab0a
DragonflyStats <kobriendublin@gmail.com>
2017-08-29 14:05:24 +0100

    Create 03-TestingNormality.R

---
+++ 03-TestingNormality.R
@@ -0,0 +1,10 @@
+nortest: Tests for Normality
+
+Five omnibus tests for testing the composite hypothesis of norm
+
+
+##########################################################
+library(nortest)
+library(ggplot2)
+
+shapiro.test(X)
