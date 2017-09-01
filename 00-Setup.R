d6d0507de8fa1fd440b45d91c8d6a3de2828611d
DragonflyStats <kobriendublin@gmail.com>
2017-09-01 21:15:25 +0100

    Update 00-Setup.R

--- 00-Setup.R
+++ 00-Setup.R
@@ -17,3 +17,6 @@ library(MASS)
 
 Sigma <- matrix(c(9,8,8,25),2,2)
 HW <- data.frame(mvrnorm(n = 5000, c(55, 170), Sigma))
+
+#####################################
+source('https://gist.githubusercontent.com/DragonflyStats/e99b44fa0b256947dd8eef60f41dedc2/raw/58d95e913c1d68578fe11a5f9fd5a22793dbe257/MA4605LabClasses.R')
