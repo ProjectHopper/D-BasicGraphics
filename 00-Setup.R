c140f3e0aea20508159d911ff8c3ba473f025cb5
DragonflyStats <kobriendublin@gmail.com>
2017-09-01 20:44:11 +0100

    Create 00-Setup.R

---
+++ 00-Setup.R
@@ -0,0 +1,10 @@
+# Capital Punishment Data Sets
+Cappun <- read.csv('https://raw.githubusercontent.com/RWorkshop/workshopdatasets/master/cappun.csv')
+
+###################
+# Superworld Data Set
+SW <- read.csv('https://raw.githubusercontent.com/RWorkshop/SuperWorld/master/Version1/Superworld.csv')
+
+###################
+# Systolic Blood Pressure
+install.packages('MethComp')
