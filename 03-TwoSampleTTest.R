2f82600afe2b3fe4031d946aeeea54ffb856ad86
DragonflyStats <kobriendublin@gmail.com>
2017-09-01 15:37:16 +0100

    Update 03-TwoSampleTTest.R

--- 03-TwoSampleTTest.R
+++ 03-TwoSampleTTest.R
@@ -12,3 +12,17 @@ var.test(X,Y)
 t.test(X,Y,var.equal=TRUE)
 
 ################################################################
+
+myTrueMean1 <- sample(95:105,1)
+myTrueMean2 <- sample(95:105,1)
+
+myTrueVars <- c(10,15,20,25,30)
+
+# Dont Look !!
+
+myPop1 <- rnorm(25000,myTrueMean1,sample(myTrueVars,1))
+
+# N.B. Same Variance of 20
+
+mySamp1 <- sample(myPop1,30)
+mySamp2 <- sample(myPop2,30)
